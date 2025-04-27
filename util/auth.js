eebbb2d8d6b7fc3266e9329885c1cd5213d83f9d
qinxl5 <qinxl5@asiainfo.com>
2025-04-27 21:05:52 +0800

    提交

---
+++ util/auth.js
@@ -0,0 +1,14 @@
+const TokenKey = 'Token';
+
+
+export function getToken() {
+    return uni.getStorageSync(TokenKey);
+}
+
+export function setToken(token) {
+    return uni.setStorageSync(TokenKey, token);
+}
+
+export function removeToken() {
+    return uni.removeStorageSync(TokenKey);
+}
