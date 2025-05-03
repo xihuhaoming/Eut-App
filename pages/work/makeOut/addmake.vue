a6a18b95745d369c0e0b1c66dcce9e413032a850
qinxl5 <qinxl5@asiainfo.com>
2025-05-03 16:28:19 +0800

    feat：发票

--- pages/work/makeOut/addmake.vue
+++ pages/work/makeOut/addmake.vue
@@ -4,8 +4,7 @@
 		<up-tabs :scrollable="false" :list="list" @click="tabclick"
 			itemStyle="padding:0 50rpx; height: 34rpx; margin:30rpx 0;" inactiveStyle="font-size: 28rpx;color: #B7C4D7;"
 			activeStyle="color: #092D5C;font-size:30rpx"></up-tabs>
-		<view class="content">
-
+		<view class="content" v-if="tabIndex == 0">
 			<view class="typeTop up-flex u-row-center">
 				<hy-btn-group :list="typeList" v-model="typeValue" @itemClick="itemClick" :cancelSelectItem="false"
 					:unSelectedStyle="{background: '#F5F7FB',color: '#5A78A0',borderColor: '#F6F8FC',borderRadius:'30rpx'}"
@@ -74,13 +73,102 @@
 					<textarea type="textarea" placeholder="请输入备注" border="surround" v-model="textValue"></textarea>
 				</view>
 			</view>
-
-
-
-
-
 			<view class="qued up-m-t-70">提交</view>
+
 		</view>
+		<view class="content1" v-if="tabIndex == 1">
+			<view class="up-p-t-20" style="background:#ffffff;">
+				<up-search class="up-m-b-20" placeholder="请输入查找内容" v-model="keyword" :showAction="false" bgColor="#F6F8FC"
+					height="40"></up-search>
+				<view class="u-flex filter u-col-center u-row-around">
+					<!-- 时间 -->
+					<up-dropdown ref="uDropdownRef" border-radius="20">
+						<up-dropdown-item title="时间">
+							<view class="slot-content">
+								<view class="slotCon up-flex up-text-center up-content-color u-row-around up-p-t-40 up-p-b-40">
+									<view class="timeView" @click="timeTan(1)">{{startTime}}</view>
+									<view class="timeView" @click="timeTan(2)">{{endTime}}</view>
+								</view>
+								<view class="slotCon up-flex up-text-center u-row-around up-p-t-40 up-p-b-40">
+									<view class="timeBtn" @click="chongzTime">重置</view>
+									<view class="timeBtn2" @click="SubmitTime">确认</view>
+								</view>
+								<!-- <up-button type="primary" @click="closeDropdown">时间</up-button> -->
+							</view>
+						</up-dropdown-item>
+						<!-- 类型 -->
+						<up-dropdown-item title="类型">
+							<view class="slot-content up-p-l-25 up-p-r-25">
+								<view class="slotCon up-text-center up-content-color up-p-t-40 up-p-b-40">
+									<!-- 任务 -->
+									<view class="typeTitle u-flex u-col-center ">
+										<view class="ttleft up-m-r-20"></view>
+										<view class="ttright">任务&通知</view>
+									</view>
+									<hy-btn-group class="up-m-t-30 up-m-l-20" :list="workList0" v-model="workValue0"
+										@itemClick="itemClick1" multiple
+										:unSelectedStyle="{background: '#F5F7FB',color: '#5A78A0',borderColor: '#F5F7FB'}"
+										:selectedStyle="{background: '#ECF1FF',color: '#3C82FE',borderColor: '#ECF1FF'}"></hy-btn-group>
+									<!-- 工作组名称 -->
+									<view class="typeTitle u-flex u-col-center up-m-t-30">
+										<view class="ttleft1 up-m-r-20"></view>
+										<view class="ttright">工作组名称</view>
+									</view>
+									<hy-btn-group class="up-m-t-30 up-m-l-20" :list="workList" v-model="workValue" @itemClick="itemClick1"
+										multiple :unSelectedStyle="{background: '#F5F7FB',color: '#5A78A0',borderColor: '#F5F7FB'}"
+										:selectedStyle="{background: '#ECF1FF',color: '#3C82FE',borderColor: '#ECF1FF'}"></hy-btn-group>
+									<!-- 工作组名称 -->
+									<view class="typeTitle u-flex u-col-center up-m-t-30">
+										<view class="ttleft1 up-m-r-20"></view>
+										<view class="ttright">工作组名称</view>
+									</view>
+									<hy-btn-group class="up-m-t-30 up-m-l-20" :list="workList2" v-model="workValue2"
+										@itemClick="itemClick1" multiple
+										:unSelectedStyle="{background: '#F5F7FB',color: '#5A78A0',borderColor: '#F5F7FB'}"
+										:selectedStyle="{background: '#ECF1FF',color: '#3C82FE',borderColor: '#ECF1FF'}"></hy-btn-group>
+								</view>
+								<view class="slotCon up-flex up-text-center u-row-around up-p-t-40 up-p-b-40">
+									<view class="timeBtn" @click="chongzTime">重置</view>
+									<view class="timeBtn2" @click="SubmitTime">确认</view>
+								</view>
+								<!-- <up-button type="primary" @click="closeDropdown">时间</up-button> -->
+							</view>
+						</up-dropdown-item>
+					</up-dropdown>
+				</view>
+			</view>
+			<view class="taskbox">
+				<view class="taskItem">
+					<view class="titemTop u-flex u-row-between up-p-b-15">
+						<view class="ttleft u-flex u-col-center">
+							<image src="/static/work/time.png"></image>
+							<view>2023.03.05 16:00</view>
+						</view>
+						<view class="ttright">待确认</view>
+					<!-- 	<view class="ttright" style="color:#FE4949">待确认</view>
+						<view class="ttright" style="color:#1CAA42">待确认</view>
+						<view class="ttright" style="color:#092D5C">已作废</view> -->
+					</view>
+					<view class="task-title up-m-t-20 bold">
+						河南微鸟科技网络有限公司
+					</view>
+					<view class="tasktext up-m-b-20" style="color:#B7C4D7">S8D00F9A0D9-ASSD </view>
+					<view class="tasktext">开票金额：<text style="color:red">28930.00</text></view>
+					<view class="tasktext">合同编码：EUT-HT-LS-1901</view>
+					<view class="tasktext">部门：数字工程中心</view>
+					<view class="tasktext">负责人：易尤特</view>
+				</view>
+			</view>
+
+
+		</view>
+		<!-- 选择时间 -->
+		<up-datetime-picker :minDate="1735660800000" :hasInput="false" class="timeView" hasInput :show="timeshow1"
+			v-model="startTime" mode="date" placeholder="开始时间" @confirm="confirm1"
+			@cancel="timeshow1 = false"></up-datetime-picker>
+		<up-datetime-picker :minDate="1735660800000" :hasInput="false" class="timeView" hasInput :show="timeshow2"
+			v-model="endTime" mode="date" placeholder="结束时间" @confirm="confirm2"
+			@cancel="timeshow2 = false"></up-datetime-picker>
 	</view>
 </template>
 
@@ -90,6 +178,8 @@
 		reactive,
 		onMounted
 	} from 'vue';
+	const tabIndex = ref(0)
+	const keyword = ref("")
 	const list = reactive([{
 			name: '发起提交'
 		},
@@ -105,8 +195,53 @@
 		label: '增值税专用发票',
 		value: 2
 	}]);
+	const workList0 = reactive([{
+		label: '任务',
+		value: 1
+	}, {
+		label: '通知',
+		value: 2
+	}]);
 	const name = ref('')
 	const textValue = ref('')
+	const workList = reactive([{
+			label: '采购流程',
+			value: 1
+		}, {
+			label: '入库流程',
+			value: 2
+		},
+		{
+			label: '合同流程',
+			value: 3
+		},
+		{
+			label: '工作流流程',
+			value: 4
+		},
+	]);
+	const workList2 = reactive([{
+			label: '采购流程',
+			value: 1
+		}, {
+			label: '入库流程',
+			value: 2
+		},
+		{
+			label: '合同流程',
+			value: 3
+		}
+	]);
+	const workValue0 = reactive([])
+	const workValue = reactive([])
+	const workValue2 = reactive([])
+	const uDropdownRef = ref(null)
+	const startTime = ref('开始时间')
+	const endTime = ref('结束时间')
+	const timeshow1 = ref(false);
+	const timeshow2 = ref(false);
+
+
 	// 选择类型Z 
 	const itemClick = (e) => {
 		console.log(e)
@@ -114,12 +249,69 @@
 	// tab切换
 	const tabclick = (e) => {
 		console.log(e)
+		tabIndex.value = e.index
+	}
+	// 关闭筛选框
+	const closeDropdown = () => {
+		// this.$refs.uDropdownRef.close();
+		uDropdownRef.value.close()
+	}
+	// 选择类型
+	const itemClick1 = (e) => {
+		console.log(e)
+	}
+	// 选择时间
+	const timeTan = (e) => {
+		console.log(e)
+		if (e == 1) {
+			timeshow1.value = true
+		} else {
+			timeshow2.value = true
+		}
+	}
+	const confirm1 = (e) => {
+		startTime.value = formatTimestamp(e.value)
+		timeshow1.value = false;
+	}
+	const confirm2 = (e) => {
+		endTime.value = formatTimestamp(e.value)
+		timeshow2.value = false;
+	}
+	// 时间重置
+	const chongzTime = () => {
+		startTime.value = "开始时间"
+		endTime.value = "结束时间"
+		workValue0.length = 0;
+		workValue.length = 0;
+		workValue2.length = 0;
+	}
+	// 时间确定
+	const SubmitTime = () => {
+		uDropdownRef.value.close()
+		workValue0.length = 0;
+		workValue.length = 0;
+		workValue2.length = 0;
+		startTime.value = "开始时间";
+		endTime.value = "结束时间";
+
+	}
+	// 转换时间戳
+	const formatTimestamp = (timestamp) => {
+		const date = new Date(timestamp);
+		const year = date.getFullYear();
+		const month = String(date.getMonth() + 1).padStart(2, '0');
+		const day = String(date.getDate()).padStart(2, '0');
+		return `${year}-${month}-${day}`;
 	}
 </script>
 
 <style setup lang="scss">
 	page {
 		background: #F6F8FA;
+	}
+
+	::v-deep .u-dropdown-item {
+		background: #fff;
 	}
 
 	::v-deep .u-tabs {
@@ -191,6 +383,96 @@
 		::v-deep .btn-group {
 			justify-content: center !important;
 			display: flex !important;
+		}
+	}
+
+	.slot-content {
+		background: #fff;
+
+		.timeView {
+			padding: 18rpx 85rpx;
+			background: #F5F7FB;
+			font-size: 24rpx;
+			color: #5A78A0;
+			border-radius: 34rpx;
+		}
+
+		.timeBtn {
+			font-size: 28rpx;
+			color: #3C82FE;
+			padding: 24rpx 120rpx;
+			background: #F6F8FC;
+			border-radius: 49rpx;
+		}
+
+		.timeBtn2 {
+			font-size: 28rpx;
+			color: #fff;
+			padding: 24rpx 120rpx;
+			background: #3C82FE;
+			border-radius: 49rpx;
+		}
+
+		.typeTitle {
+			font-size: 30rpx;
+			color: #092D5C;
+
+			.ttleft {
+				width: 12rpx;
+				height: 12rpx;
+				background: linear-gradient(53deg, #FFC46F 0%, #FEAC49 100%);
+				box-shadow: 0rpx 2rpx 6rpx 0rpx #FEAC49;
+			}
+
+			.ttleft1 {
+				width: 12rpx;
+				height: 12rpx;
+				background: linear-gradient(53deg, #6FB8FF 0%, #3C82FE 100%);
+				box-shadow: 0rpx 2rpx 6rpx 0rpx rgba(60, 130, 254, 0.3);
+			}
+		}
+
+	}
+
+	.taskbox {
+		padding: 30rpx;
+	}
+
+	.taskItem {
+		background: #FFFFFF;
+		box-shadow: 0rpx 6rpx 26rpx 1rpx rgba(138, 139, 143, 0.16);
+		border-radius: 10rpx;
+		padding: 30rpx;
+
+		.titemTop {
+			border-bottom: 1rpx solid #F5F5F5;
+
+			.ttleft {
+				font-size: 26rpx;
+				color: #B7C4D7;
+
+				image {
+					width: 30rpx;
+					height: 30rpx;
+					margin-right: 10rpx;
+				}
+			}
+
+			.ttright {
+				font-size: 26rpx;
+				color: #3C82FE;
+			}
+		}
+
+		.task-title {
+			font-size: 36rpx;
+			color: #092D5C;
+		}
+
+		.tasktext {
+			font-size: 26rpx;
+			color: #5A78A0;
+			line-height: 50rpx;
 		}
 	}
 </style>
---
+++ pages/work/makeOut/makeDetile.vue
@@ -0,0 +1,217 @@
+<template>
+	<!-- 开票详情 -->
+	<view class="">
+		<up-navbar title="开票详情" :placeholder="true" :autoBack="true" />
+		<view class="conTop">
+			<view class="time">2023.02.12 16:00:00</view>
+			<view class="title bold">易尤特提交的开票申请</view>
+			<view class="zhongx">嘉兴运营中心</view>
+			<view class="type">已开票</view>
+		</view>
+		<view class="fengx"></view>
+		<view class="conZho">
+			<view class="xinx bold  up-m-b-20">申请信息</view>
+			<view class="zhoItem u-flex u-col-center">
+				<view class="zhoLf">系统编号</view>
+				<view class="zhoRi up-m-l-30">20240812090816363960</view>
+			</view>
+			<view class="zhoItem u-flex u-col-center">
+				<view class="zhoLf">合同编号</view>
+				<view class="zhoRi up-m-l-30" style="color:#3C82FE">EUT-HT-LS-1901</view>
+			</view>
+			<view class="zhoItem">
+				<view class="zhoLf">开票信息</view>
+				<view class="zhotable up-m-l-30 up-m-t-20">
+					<view class="tbaletop u-flex u-col-center u-row-around">
+						<view class="ttleft">产品</view>
+						<view class="ttright">开票金额</view>
+					</view>
+					<view class="tbalebot u-flex u-col-center u-row-around">
+						<view class="ttleft">CE-EMC</view>
+						<view class="ttright">26000</view>
+					</view>
+				</view>
+			</view>
+			<view class="zhoItem u-flex u-col-center">
+				<view class="zhoLf">发票类型</view>
+				<view class="zhoRi up-m-l-30">增值税专用发票</view>
+			</view>
+			<view class="zhoItem u-flex u-col-center">
+				<view class="zhoLf">税号</view>
+				<view class="zhoRi up-m-l-30">72385940304590</view>
+			</view>
+			<view class="zhoItem u-flex u-col-center">
+				<view class="zhoLf">地址</view>
+				<view class="zhoRi up-m-l-30">浙江省嘉兴市嘉善县大云镇云寺西路268号</view>
+			</view>
+			<view class="zhoItem u-flex u-col-center">
+				<view class="zhoLf">电话</view>
+				<view class="zhoRi up-m-l-30">72394830</view>
+			</view>
+			<view class="zhoItem u-flex u-col-center">
+				<view class="zhoLf">开户行</view>
+				<view class="zhoRi up-m-l-30">中国银行</view>
+			</view>
+			<view class="zhoItem u-flex u-col-center">
+				<view class="zhoLf">银行账号</view>
+				<view class="zhoRi up-m-l-30">72394830备份</view>
+			</view>
+			<view class="zhoItem u-flex u-col-center">
+				<view class="zhoLf">开票公司</view>
+				<view class="zhoRi up-m-l-30">中国银行备份</view>
+			</view>
+			<view class="zhoItem u-flex u-col-center">
+				<view class="zhoLf">开票接收邮箱</view>
+				<view class="zhoRi up-m-l-30">72394830备份 2</view>
+			</view>
+			<view class="zhoItem u-flex u-col-center">
+				<view class="zhoLf">开票金额</view>
+				<view class="zhoRi up-m-l-30" style="color:#FE4949">5000</view>
+			</view>
+			<view class="zhoItem u-flex u-col-center">
+				<view class="zhoLf">开票备注</view>
+				<view class="zhoRi up-m-l-30"></view>
+			</view>
+		</view>
+		<view class="fengx"></view>
+		<view class="conZho">
+			<view class="xinx bold up-m-b-20">开票信息</view>
+			<view class="zhoItem u-flex u-col-center">
+				<view class="zhoLf">发票代码</view>
+				<view class="zhoRi up-m-l-30">20240812090816363960</view>
+			</view>
+			<view class="zhoItem u-flex u-col-center">
+				<view class="zhoLf">发票号码</view>
+				<view class="zhoRi up-m-l-30">20240812090816363960</view>
+			</view>
+			<view class="zhoItem u-flex u-col-center">
+				<view class="zhoLf">开票人</view>
+				<view class="zhoRi up-m-l-30">20240812090816363960</view>
+			</view>
+			<view class="zhoItem u-flex u-col-center">
+				<view class="zhoLf">开票时间</view>
+				<view class="zhoRi up-m-l-30">20240812090816363960</view>
+			</view>
+			<view class="zhoItem u-flex u-col-center">
+				<view class="zhoLf">备注</view>
+				<view class="zhoRi up-m-l-30">20240812090816363960</view>
+			</view>
+		</view>
+		<view class="fengx"></view>
+		<view class="conZho">
+			<view class="xinx bold up-m-b-20">流程及办理</view>
+			<view class="liucItem u-flex">
+				<image src="/static/logo.png"></image>
+				<view class="liucRight u-flex up-m-l-20 u-row-between">
+					<view class="">
+						<view class="lrleft">发起申请</view>
+						<view class="lrlbot">发起人</view>
+					</view>
+					<view class="lrleft">2023.03.26 16:00:00</view>
+				</view>
+			</view>
+			<view class="shux up-m-l-50 up-m-t-20"></view>
+		</view>
+	</view>
+</template>
+
+<script setup>
+</script>
+
+<style scoped lang="scss">
+	page {}
+	.liucItem{
+		image{
+			width:100rpx;
+			height:100rpx;
+			border-radius:50%;
+			display:block;
+		}
+		.liucRight{
+			width:70vw;
+			.lrleft{
+				font-size: 26rpx;
+				color: #B7C4D7;
+			}
+			.lrlbot{
+				font-size: 28rpx;
+				color: #092D5C;
+			}
+		}
+	}
+	.shux{
+		width:1rpx;
+		height:60rpx;
+		border:3rpx solid #B7C4D7;
+	}
+	.conZho {
+		padding: 30rpx 35rpx;
+		.zhoItem {
+			padding: 10rpx 0;
+		}
+
+		.xinx {
+			font-size: 36rpx;
+			color: #092D5C;
+		}
+
+		.zhoLf {
+			font-size: 26rpx;
+			color: #5A78A0;
+			width: 25vw;
+		}
+
+		.zhoRi {
+			font-size: 28rpx;
+			color: #092D5C;
+		}
+
+		.zhotable {
+			// border: 1rpx solid #979797;
+			box-shadow: inset 0 0 0 0.5rpx #979797;
+
+			.tbaletop {
+				height: 88rpx;
+				background-color: #3C82FE;
+				color: #fff;
+			}
+
+			.tbalebot {
+				height: 88rpx;
+			}
+		}
+
+
+	}
+
+	.conTop {
+		padding: 30rpx 35rpx;
+
+		.time {
+			font-size: 26rpx;
+			color: #B7C4D7;
+		}
+
+		.title {
+			font-size: 36rpx;
+			color: #092D5C;
+		}
+
+		.zhongx {
+			font-size: 26rpx;
+			color: #5A78A0;
+			line-height: 2;
+		}
+
+		.type {
+			font-size: 28rpx;
+			color: #1CAA42;
+		}
+	}
+
+	.fengx {
+		width: 100vw;
+		height: 20rpx;
+		background: #F6F8FA;
+	}
+</style>
